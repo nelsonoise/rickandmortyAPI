ac14d5f9d5fd235e4c3eb2edca30ce2617f8ff7d
carlos.camacho <carlos.camacho@patronato.unam.mx>
2026-03-27 19:14:28 -0600

    initial commit

--- index.php
+++ index.php
@@ -2,19 +2,54 @@
 require_once 'RickAndMortyService.php';
 
 $service = new RickAndMortyService();
-
-// Intentamos obtener la primera página de personajes
 $data = $service->getCharacters(1);
+?>
 
-if (isset($data['error'])) {
-    echo "Hubo un problema: " . $data['error'];
-} else {
-    echo "<h2>Personajes encontrados:</h2>";
-    foreach ($data['results'] as $character) {
-        echo "<div>";
-        echo "<strong>Nombre:</strong> " . $character['name'] . "<br>";
-        echo "<strong>Especie:</strong> " . $character['species'] . "<br>";
-        echo "<hr>";
-        echo "</div>";
-    }
-}
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rick and Morty Cast</title>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+
+<body>
+
+    <h1>Rick and Morty API</h1>
+
+    <div class="container">
+        <?php if (isset($data['results'])): ?>
+            <?php foreach ($data['results'] as $char): ?>
+                <div class="card">
+                    <img src="<?= $char['image'] ?>" alt="<?= $char['name'] ?>">
+                    <div class="card-info">
+                        <div class="section">
+                            <h2><?= $char['name'] ?></h2>
+                            <span class="status">
+                                <span class="status-dot <?= strtolower($char['status']) ?>"></span>
+                                <?= $char['status'] ?> - <?= $char['species'] ?>
+                            </span>
+                        </div>
+
+                        <div class="section">
+                            <span class="label">Última ubicación conocida:</span><br>
+                            <span class="value"><?= $char['location']['name'] ?></span>
+                        </div>
+
+                        <div class="section">
+                            <span class="label">Primera vez visto en:</span><br>
+                            <span class="value"><?= $char['origin']['name'] ?></span>
+                        </div>
+                    </div>
+                </div>
+            <?php endforeach; ?>
+        <?php else: ?>
+            <p>No hay datos disponibles.</p>
+        <?php endif; ?>
+    </div>
+
+</body>
+
+</html>
